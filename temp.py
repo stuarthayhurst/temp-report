7c31f38c93412d3dac3c8b70d0896614c83e5f27
Dragon8oy <stuart.a.hayhurst@gmail.com>
2019-03-07 21:30:46 +0000

    Updated graph.py, changed logfile format, added more configs, corrected spellings, updated gitignore and allowed using configs for the graph script

--- temp.py
+++ temp.py
@@ -1,16 +1,16 @@
 import smtplib, datetime, time, csv, sys, os
 import graph
-from w1thermsensor import W1ThermSensor
+#from w1thermsensor import W1ThermSensor
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 #Variables for email and the sensor
-email_recipents = ['example@gmail.com', 'example@gmail.com'] #Set recipent emails here if use_csv_recipent = 0
+email_recipients = ['example@gmail.com', 'example@gmail.com'] #Set recipient emails here if use_csv_recipient = 0
 email_sender = 'example@gmail.com' #Set sender email here if use_csv_sender = 0
 password = 'password' #Set sender email password here if use_csv_sender = 0
 last_email_time = datetime.datetime(1970, 1, 1, 0, 0)
 email_time_diff = 0
-sensor = W1ThermSensor()
+#sensor = W1ThermSensor()
 
 #See config.csv for a config file. Use python3 temp.py -c to generate a new one
 
@@ -22,8 +22,10 @@ def updateConfig(force):
     ['gap', '3600'],
     ['threshold_max', '30'],
     ['threshold_min', '-1'],
-    ['use_csv_recipent', '1'],
+    ['use_csv_recipient', '1'],
     ['use_csv_sender', '1'],
+    ['graph_point_count', '12'],
+    ['graph_interval', '5'],
     ]
 
   if force == 'y':
@@ -70,13 +72,21 @@ def updateConfig(force):
             global threshold_min
             threshold_min = int(row[1])
             config_count += 1
-          elif row[0] == 'use_csv_recipent':
-            global use_csv_recipent
-            use_csv_recipent = int(row[1])
-            config_count += 1
+          elif row[0] == 'use_csv_recipient':
+            global use_csv_recipient
+            use_csv_recipient = int(row[1])
+            config_count += 1         
           elif row[0] == 'use_csv_sender':
             global use_csv_sender
             use_csv_sender = int(row[1])
+            config_count += 1
+          elif row[0] == 'graph_point_count':
+            global graph_point_count
+            graph_point_count = int(row[1])
+            config_count += 1
+          elif row[0] == 'graph_interval':
+            global graph_interval
+            graph_interval = int(row[1])
             config_count += 1
           else:
             print('Invalid config line detected\n')
@@ -87,11 +97,11 @@ def connectToServer():
   global server
   server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
   server.connect('smtp.gmail.com', 465)
-  print('Connected to server as ' + email_sender)
+  print('Connected to server')
   server.login(email_sender, password)
-  print('Logged in successfully\n')
+  print('Logged in successfully as ' + email_sender + '\n')
 
-def updateRecipents():
+def updateRecipients():
   try:
     with open('addresses.csv') as csv_file:
       csv_reader = csv.reader(csv_file, delimiter=',')
@@ -102,12 +112,12 @@ def updateRecipents():
               line_count += 1
           else:
               print(f'\t{row[0]}')
-              if len(email_recipents) + 1 == line_count:
-                email_recipents.append(str(row[0]))
+              if len(email_recipients) + 1 == line_count:
+                email_recipients.append(str(row[0]))
               else:
-                email_recipents[line_count - 1] = str(row[0])
+                email_recipients[line_count - 1] = str(row[0])
               line_count += 1
-    print(f'\nUsing CSV for recipent addresses, processed {line_count - 1} addresses, {line_count} lines')
+    print(f'\nUsing CSV for recipient addresses, processed {line_count - 1} addresses, {line_count} lines')
   except FileNotFoundError:
     print("\nNo address file found, starting address editor: \n")
     import data_edit
@@ -207,10 +217,10 @@ def sendMessage():
   #Sends the message if the time is ok
   if email_time_diff >= gap:
     #Sends an email to the addresses in the array
-    for x in range(0, len(email_recipents)):
-      server.sendmail(email_sender, email_recipents[x], msg.as_string())
-      print('Email sent to ' + str(email_recipents[x]))
-    print('Sent email to ' + str(len(email_recipents)) + ' addresses')
+    for x in range(0, len(email_recipients)):
+      server.sendmail(email_sender, email_recipients[x], msg.as_string())
+      print('Email sent to ' + str(email_recipients[x]))
+    print('Sent email to ' + str(len(email_recipients)) + ' addresses')
     print('Email was last sent ' + str(email_time_diff) + ' seconds ago')
     last_email_time = currTime
   else:
@@ -223,7 +233,7 @@ def measureTemp():
   #Measures the temperature
   global temp
   print('Reading temperature:')
-  temp = sensor.get_temperature()
+  temp = 30#sensor.get_temperature()
   currTime = datetime.datetime.now()
   print('The temperature is ' + str(temp) + '°C at ' + str(currTime.strftime("%H:%M:%S")) + '\n')
 
@@ -241,7 +251,9 @@ def logTemp():
     print('Log created\n')
 
   currTime = datetime.datetime.now()
-  logLine = 'Temperature: ' + str(temp) + '°C at ' + str(currTime.strftime("%c"))
+  
+  #logLine = 'Temperature: ' + str(temp) + '°C at ' + str(currTime.strftime("%c"))
+  logLine = '[' + str(currTime.strftime("%c")) + '] Temperature: ' + str(temp) + '°C'
   changes = [
     [logLine],                                      
     ]
@@ -253,7 +265,7 @@ sys.argv.append(0)
 if sys.argv[1] == '-h' or sys.argv[1] == '--help':
   print('Options:')
   print('	-h | --help    : Display the help menu')
-  print('	-a | --addresses : Add, remove, view or edit recipent email addresses')
+  print('	-a | --addresses : Add, remove, view or edit recipient email addresses')
   print('	-p | --password : Change the password for the sender email')
   print('	-s | --sender : Change the address of the sender email')
   exit()
@@ -280,12 +292,12 @@ while counter == 0:
   logTemp()
   #Update addresses and credentials
   if temp >= threshold_max or temp <= threshold_min:
-    if use_csv_recipent == 1:
-      updateRecipents()
+    if use_csv_recipient == 1:
+      updateRecipients()
     if use_csv_sender == 1:
       updateSender()
     #Create message contents
-    generateGraph()
+    graph.generateGraph(graph_point_count, graph_interval)
     updateMessage()
     #Send the message
     connectToServer()
