e2d4ea6d5bbfbe4f5387ccf5f722d2e5c216b2ce
Dragon8oy <stuart.a.hayhurst@gmail.com>
2019-04-06 17:17:09 +0100

    Made autostart work when it's called anywhere

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 while [[ "$#" -gt 0 ]]; do case $1 in
-  -t|--temp) echo "Starting temperature monitor:"; tmux new-session -d -s temp_report 'cd /home/pi/temp-report && /usr/bin/python3 /home/pi/temp-report/temp.py; exec bash -i';;
-  -r|--reply) echo "Starting email listener and reply bot:"; tmux new-session -d -s temp_listener 'cd /home/pi/temp-report && /usr/bin/python3 /home/pi/temp-report/listener.py; exec bash -i';;
-  -l|--logger) echo "Starting logger:"; tmux new-session -d -s temp_log 'cd /home/pi/temp-report && /usr/bin/python3 /home/pi/temp-report/log.py; exec bash -i';;
+  -t|--temp) echo "Starting temperature monitor:"; tmux new-session -d -s temp_report 'cd '$DIR' && /usr/bin/python3 '$DIR'/temp.py; exec bash -i';;
+  -r|--reply) echo "Starting email listener and reply bot:"; tmux new-session -d -s temp_listener 'cd '$DIR' && /usr/bin/python3 '$DIR'/listener.py; exec bash -i';;
+  -l|--logger) echo "Starting logger:"; tmux new-session -d -s temp_log 'cd '$DIR' && /usr/bin/python3 '$DIR'/log.py; exec bash -i';;
   *) echo "Unknown parameter passed: $1"; exit 1;;
 esac; shift; done
