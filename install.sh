29611ed9e3fee4e28b07e82e0a9a355540741a19
Dragon8oy <stuart.a.hayhurst@gmail.com>
2019-04-06 17:23:47 +0100

    Forced use of tmux, removed requirment to install in /home/pi/temp-report and corrected some errors and typos

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #Function to make swap space
 addswap() {
@@ -19,9 +20,8 @@ sudo echo ""
 sudo apt-get install tmux -y
 
 if ! { [ "$TERM" = "screen" ] && [ -n "$TMUX" ]; } then
-  echo "You are not running the installer with tmux"
-  echo "We STRONGLY reccomend running the install in tmux"
-  echo "Press etner to continue or CTRL + C to exit"
+  tmux new-session -d -s temp_installer '/bin/bash '$DIR'/install.sh'
+  exit
 fi
 
 PYVER=$(python3 -V 2>&1 | grep -Po '(?<=Python )(.+)')
@@ -35,17 +35,6 @@ then
   echo "Sufficient Python version found"
 else
   echo "Please set /usr/bin/python3 to Python 3.6+"
-  exit
-fi
-
-#Check folder location
-LOCATION=$(pwd)
-
-if [ "$LOCATION" == "/home/ratus5/Downloads/temp-report" ]
-then
-  echo "Correct install location found"
-else
-  echo "Please move the folder to /home/pi, so that the path to this file is /home/pi/temp-report/install.sh and run this script there"
   exit
 fi
 
@@ -75,11 +64,11 @@ sudo pip3 install w1thermsensor matplotlib pillow
 sudo cp install/temp-* /etc/systemd/system/
 sudo systemctl start temp-report
 sudo systemctl start temp-listener
-#sudo systemctl start temp-log
+sudo systemctl start temp-log
 
 sudo systemctl enable temp-report
 sudo systemctl enable temp-listener
-#sudo systemctl enable temp-log
+sudo systemctl enable temp-log
 
 if grep -Fxq "dtoverlay=w1-gpio" /boot/config.txt
 then
@@ -94,10 +83,10 @@ fi
 python3 temp.py -c
 
 #Add user credentials and tell them how to add addresses
-echo "Use 'python3.6 temp.py -s' to set a sender email address"
-echo "Use 'python3.6 temp.py -n' to set a sender name"
-echo "Use 'python3.6 temp.py -p' to update the password for the sender address"
-echo "Use 'python3.6 temp.py -a' to add or edit addresses on the mailing list"
+echo "Use 'python3 temp.py -s' to set a sender email address"
+echo "Use 'python3 temp.py -n' to set a sender name"
+echo "Use 'python3 temp.py -p' to update the password for the sender address"
+echo "Use 'python3 temp.py -a' to add or edit addresses on the mailing list"
 
 #Cleanup
 MEM=$(grep MemTotal /proc/meminfo | awk '{print $2}' | xargs -I {} echo "scale=4; {}/1024^2" | bc)
