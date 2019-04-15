50bb8b92e28c077a85405e6943eb3b61a66e1bec
Dragon8oy <stuart.a.hayhurst@gmail.com>
2019-04-15 18:44:28 +0100

    Began work on webserver frontend

---
+++ web/tempweb.py
@@ -0,0 +1,16 @@
+
+from flask import Flask, render_template, url_for
+#from w1thermsensor import W1ThermSensor
+import flask
+app = Flask(__name__, static_url_path='/static')
+#sensor = W1ThermSensor()
+tempVer= '0.0.5'
+
+@app.route('/')
+def main(flaskVer= flask.__version__, tempVer= tempVer):
+    def measureTemp():
+        return 'Test'
+    return render_template('tempreport.html', flaskVer=flaskVer, tempVer=tempVer, measureTemp=measureTemp)
+
+if __name__ == "__main__":
+    app.run()
