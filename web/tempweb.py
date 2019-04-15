36a0e2d7b1ad2ff88991aff94b0eb013a77e5f9d
Dragon8oy <stuart.a.hayhurst@gmail.com>
2019-04-15 20:18:15 +0100

    Added automatic temperature updating

--- web/tempweb.py
+++ web/tempweb.py
@@ -1,15 +1,16 @@
 
 from flask import Flask, render_template, url_for
-#from w1thermsensor import W1ThermSensor
+from w1thermsensor import W1ThermSensor
+import random
 import flask
 app = Flask(__name__, static_url_path='/static')
-#sensor = W1ThermSensor()
-tempVer= '0.0.7'
+sensor = W1ThermSensor()
+tempVer= '0.0.8'
 
 @app.route('/')
 def main(flaskVer= flask.__version__, tempVer= tempVer):
     def measureTemp():
-        return str(sensor.get_temperature()) + '°C'
+        return sensor.get_temperature() + '°C'
     return render_template('tempreport.html', flaskVer=flaskVer, tempVer=tempVer, measureTemp=measureTemp)
 
 if __name__ == "__main__":
