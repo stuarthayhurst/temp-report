a8da46c650fc52a556e6363d0b1ada80a4bbd641
Dragon8oy <stuart.a.hayhurst@gmail.com>
2019-04-15 19:06:24 +0100

    Prepared temperature probe

--- web/tempweb.py
+++ web/tempweb.py
@@ -4,12 +4,13 @@ from flask import Flask, render_template, url_for
 import flask
 app = Flask(__name__, static_url_path='/static')
 #sensor = W1ThermSensor()
-tempVer= '0.0.5'
+tempVer= '0.0.6'
 
 @app.route('/')
 def main(flaskVer= flask.__version__, tempVer= tempVer):
     def measureTemp():
         return 'Test'
+        return sensor.get_temperature()
     return render_template('tempreport.html', flaskVer=flaskVer, tempVer=tempVer, measureTemp=measureTemp)
 
 if __name__ == "__main__":
