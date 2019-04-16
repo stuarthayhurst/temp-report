3ef9543a03593529e21f11c569f6bd7ca31967f2
Dragon8oy <stuart.a.hayhurst@gmail.com>
2019-04-16 16:25:07 +0100

    Added titles to the log and graph, with line count and point count respectively

--- web/tempweb.py
+++ web/tempweb.py
@@ -18,17 +18,23 @@ tempWebVer = tempreport.readLine(readme, 'keyword', 'Temp-web', char = ': ')
 tempVer = tempreport.readLine(readme, 'keyword', 'Temp-report', char = ': ')
 
 shutil.copy2(parDir + '/temps.log', currDir + '/temps.log')
-graph.generateGraph(tempreport.readCSVLine(parDir + '/data/config.csv', 2, 'keyword', 'graph_point_count', var_type = 'int'))
+graphPointCount = tempreport.readCSVLine(parDir + '/data/config.csv', 2, 'keyword', 'graph_point_count', var_type = 'int')
+graph.generateGraph(graphPointCount)
 shutil.move(currDir + '/temps.log', currDir + '/static/temps.log')
 shutil.move(currDir + '/graph.png', currDir + '/static/graph.png')
 
+with open(currDir + '/static/temps.log', "r") as f:
+    lineCount = len(f.readlines())
+
 @app.route('/')
-def main(flaskVer=flask.__version__, tempWebVer=tempWebVer, tempVer=tempVer):
+def main(flaskVer=flask.__version__, tempWebVer=tempWebVer, tempVer=tempVer, lineCount=lineCount, pointCount=graphPointCount):
     def measureTemp():
         return random.randint(1,30)#str(sensor.get_temperature()) + '°C'
     with open(currDir + '/static/temps.log', "r") as f:
         logContent = f.read()
-    return render_template('tempreport.html', flaskVer=flaskVer, tempWebVer=tempWebVer, tempVer=tempVer, measureTemp=measureTemp, logContent=logContent)
+        logContent = logContent.rsplit('\n', 1)
+        logContent = ''.join(logContent)
+    return render_template('tempreport.html', flaskVer=flaskVer, tempWebVer=tempWebVer, tempVer=tempVer, measureTemp=measureTemp, logContent=logContent, lineCount=lineCount, pointCount=pointCount)
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0', port= 5000)
