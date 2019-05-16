41483bb5c5be65e536cc5ebd3a6cb976acbde8de
Dragon8oy <stuart.a.hayhurst@gmail.com>
2019-05-16 20:58:39 +0100

    Fixed permissions when starting webpage

--- autostart.sh
+++ autostart.sh
@@ -5,6 +5,6 @@ while [[ "$#" -gt 0 ]]; do case $1 in
   -t|--temp) echo "Starting temperature monitor:"; tmux new-session -d -s temp_report 'cd '$DIR' && /usr/bin/python3 '$DIR'/temp.py; exec bash -i';;
   -r|--reply) echo "Starting email listener and reply bot:"; tmux new-session -d -s temp_listener 'cd '$DIR' && /usr/bin/python3 '$DIR'/listener.py; exec bash -i';;
   -l|--logger) echo "Starting logger:"; tmux new-session -d -s temp_log 'cd '$DIR' && /usr/bin/python3 '$DIR'/log.py; exec bash -i';;
-  -w|--web) echo "Starting webpage:"; tmux new-session -d -s temp_web 'cd '$DIR'/web && /usr/bin/python3 '$DIR'/web/tempweb.py; exec bash -i';;
+  -w|--web) echo "Starting webpage:"; tmux new-session -d -s temp_web 'cd '$DIR'/web && sudo /usr/bin/python3 '$DIR'/web/tempweb.py; exec bash -i';;
   *) echo "Unknown parameter passed: $1"; exit 1;;
 esac; shift; done
