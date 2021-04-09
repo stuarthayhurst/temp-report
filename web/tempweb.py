68c40da1f27c660b7252b2881ec636dc361cc2ca
Dragon8oy <stuart.a.hayhurst@gmail.com>
2021-04-09 21:03:19 +0100

    Refactored webpage code

--- web/tempweb.py
+++ web/tempweb.py
@@ -16,78 +16,64 @@ sys.path.insert(0,parDir)
 import graph, tempreport
 
 #Load config
+print('Waiting for config...')
 while os.path.isfile('../data/config.py') == False:
   time.sleep(1)
 sys.path.insert(1, '../data/')
 import config
 print("Loaded config")
 
-#Generate / prepare some website assets
-shutil.copy2(parDir + '/temps.log', currDir + '/temps.log')
-graph.generateGraph(config.graph_point_count, config.area_name)
-shutil.move(currDir + '/temps.log', currDir + '/static/temps.log')
-shutil.move(currDir + '/graph.png', currDir + '/static/graph.png')
+#Recreate symlinks for access to the log for webpage and graph.py
+os.remove(currDir + '/temps.log')
+os.remove(currDir + '/static/temps.log')
+os.symlink(parDir + '/temps.log', currDir + '/temps.log')
+os.symlink(parDir + '/temps.log', currDir + '/static/temps.log')
 
 @app.route('/', methods = ['POST','GET'])
 def main(flaskVer=flask.__version__, pointCount=config.graph_point_count, cpu=cpu):
     def measureTemp(mode):
         if mode == 'temp':
-            value = str(tempreport.measureTemp()) + '°C'
-            print('Updated Temperature')
+            return str(tempreport.measureTemp())
         elif mode == 'time':
-            value = datetime.datetime.now().strftime("%H:%M:%S")
-            print('Updated Time')
-        return value
+            return datetime.datetime.now().strftime("%H:%M:%S")
 
-    def maxTemp(mode):
-        if mode == 'temp':
-            value = str(tempreport.readCSVLine(parDir + '/data/temp-records.csv', 2, 'keyword', 'max')) + '°C'
-            print('Updated Max Temperature')
-        elif mode == 'time':
-            value = str(tempreport.readCSVLine(parDir + '/data/temp-records.csv', 3, 'keyword', 'max'))
-            print('Updated Max Temperature Time')
-        return value
-
-    def minTemp(mode):
-        if mode == 'temp':
-            value = str(tempreport.readCSVLine(parDir + '/data/temp-records.csv', 2, 'keyword', 'min')) + '°C'
-            print('Updated Max Temperature')
-        elif mode == 'time':
-            value = str(tempreport.readCSVLine(parDir + '/data/temp-records.csv', 3, 'keyword', 'min'))
-            print('Updated Max Temperature Time')
-        return value
-
+    def getTemp(maxOrMin, info):
+        if info == 'temp':
+            targetPosition = 2
+        else:
+            targetPosition = 3
+        return str(tempreport.readCSVLine(parDir + '/data/temp-records.csv', targetPosition, 'keyword', info))
 
     with open(currDir + '/static/temps.log', "r", encoding='utf-8') as f:
+        #Save log contents
         logContent = f.read()
         logContent = logContent.rsplit('\n', 1)
         logContent = ''.join(logContent)
 
-    with open(currDir + '/static/temps.log', "r", encoding='utf-8') as f:
+        #Reset file pointer to get number of lines
+        f.seek(0)
         lineCount = len(f.readlines())
         print('Found ' + str(lineCount) + ' lines')
 
+    #Use requested number of points, if specified
     if request.method == 'POST':
         result = request.form
         pointCount = result['pointsrequested']
 
-    shutil.copy2(parDir + '/temps.log', currDir + '/temps.log')
     graph.generateGraph(int(pointCount), config.area_name)
     print(f"Drew graph with {pointCount} points")
-    shutil.move(currDir + '/temps.log', currDir + '/static/temps.log')
+
     try:
         with open(currDir + '/graph.png', 'rb') as ImageData:
             graphImageData = base64.b64encode(ImageData.read())
             graphImageData = graphImageData.decode('utf-8')
     except FileNotFoundError:
         graphImageData = ""
-        print("File Not found")
-
+        print("File not found")
 
     print('Updated Files')
 
-
-    return render_template('tempreport.html', graphImageData=graphImageData, measureTemp=measureTemp, maxTemp=maxTemp, minTemp=minTemp, logContent=logContent, lineCount=lineCount, pointCount=pointCount, cpuTemp=str(cpu.temperature) + '°C')
+    return render_template('tempreport.html', graphImageData=graphImageData, measureTemp=measureTemp, getTemp=getTemp, logContent=logContent, lineCount=lineCount, pointCount=pointCount, cpuTemp=str(cpu.temperature) + '°C')
 
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0', port= 80)
+    app.run(host='0.0.0.0', port=80)
