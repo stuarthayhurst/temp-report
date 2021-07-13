71fd5e35535a44442fe41b759071936e43113574
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2021-07-13 21:11:55 +0100

    Updated the path, again

--- install/autostart.sh
+++ install/autostart.sh
@@ -4,7 +4,7 @@ cd "${workDir}/../" || exit 1
 workDir="$(pwd)"
 
 while [[ "$#" -gt 0 ]]; do case $1 in
-  -t|--temp) echo "Starting temperature monitor:"; tmux new-session -d -s temp_report "cd $workDir && /usr/bin/python3 $workDir/../tempreport.py; exec bash -i";;
+  -t|--temp) echo "Starting temperature monitor:"; tmux new-session -d -s temp_report "cd $workDir && /usr/bin/python3 $workDir/tempreport.py; exec bash -i";;
   -r|--reply) echo "Starting email listener and reply bot:"; tmux new-session -d -s temp_listener "cd $workDir && /usr/bin/python3 $workDir/listener.py; exec bash -i";;
   -l|--logger) echo "Starting logger:"; tmux new-session -d -s temp_log "cd $workDir && /usr/bin/python3 $workDir/log.py; exec bash -i";;
   -w|--web) echo "Starting webpage:"; tmux new-session -d -s temp_web "cd $workDir/web && sudo /usr/bin/python3 $workDir/web/tempweb.py; exec bash -i";;
